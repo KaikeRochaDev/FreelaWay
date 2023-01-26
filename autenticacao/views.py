4fe820bba634fb3340c79429d4211c410605e984
KaikeRochaDev <kaikerochadev@gmail.com>
2023-01-26 18:29:28 -0300

    creating, validating account login and logout url

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib import auth
 
 
 def cadastro(request):
     if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'cadastro.html')
     elif request.method == "POST":
         username = request.POST.get('username')
@@ -42,4 +45,23 @@ def cadastro(request):
 
 
 def logar(request):
-    return HttpResponse('Olá, você está no logar')
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'logar.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('password')
+        
+        usuario = auth.authenticate(username=username, password=senha)
+        
+        if not usuario:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect('/auth/logar')
+        else:
+            auth.login(request, usuario)
+            return redirect('/')
+        
+def sair(request):
+    auth.logout(request)
+    return redirect('/auth/logar')
