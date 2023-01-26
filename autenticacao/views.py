d0425250d65fae5dd2c2743515f8e7ae5975bfa1
KaikeRochaDev <kaikerochadev@gmail.com>
2023-01-26 18:12:49 -0300

    fixing validation errors

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,43 +1,45 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+
 
 def cadastro(request):
-    if request.method == 'GET':
-        return render(request, 'cadastro.html') 
-    elif request.method == 'POST':
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
         username = request.POST.get('username')
         senha = request.POST.get('password')
         confirmar_senha = request.POST.get('confirm-password')
         
-    if senha != confirmar_senha:
-        # As senhas não coincidem
-        return redirect('/auth/cadastro')
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não coincidem')
+            return redirect('/auth/cadastro')
     
-    if senha <= 5:
-        # Sua senha precisa ter 6 ou mais caracteres
-        return redirect('/auth/cadastro')
-    
-    if len(username.strip() == '') or len(senha.strip() == '') or len(confirmar_senha.strip() == ''):
-        # Preencha todos os campos
-        return redirect('/auth/cadastro')
-    
-    user = User.objects.filter(username=username)
-    
-    if user.exists():
-        # Esse usuário já existe
-        return redirect('/auth/cadastro')
-    
-    try:
-        user = User.objects.create_user(username=username, senha=senha, confirmar_senha=confirmar_senha)
+        if len(username.strip()) == 0 or len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/auth/cadastro')
         
-        user.save()
-        # cadastro realizado com sucesso
-        return redirect('/auth/logar')
-    except:
-        # Erro interno no sistema
-        return redirect('auth/cadastro')
+        if len(senha) <= 5:
+            messages.add_message(request, constants.ERROR, 'A senha deve conter 6 ou mais caracteres')
+            return redirect('/auth/cadastro')
+        
+        user = User.objects.filter(username=username)
+        
+        if user.exists():
+            messages.add_message(request, constants.ERROR,'Já existe um usuário com esse username')
+            return redirect('/auth/cadastro')
+        
+        try:
+            user = User.objects.create_user(username=username, password=senha)
+            user.save()
+            messages.add_message(request, constants.SUCCESS, 'Usuário criado com sucesso')
+            return redirect('/auth/logar')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            return redirect('/auth/cadastro')
+
 
 def logar(request):
     return HttpResponse('Olá, você está no logar')
-
